164ea20cc86ae2751ad29ab3fa9dea994394632d
Trey Crossley <treycrossley@gmail.com>
2020-07-19 13:29:11 -0700

    server changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('./dist/'));
 
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'dist/index.html' }
+  res.sendFile('index.html', { root: 'dist/' }
   );
 });
 
