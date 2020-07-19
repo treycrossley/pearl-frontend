4cf3d28416d9816d348d3213e0fb654719c667e4
Trey Crossley <treycrossley@gmail.com>
2020-07-19 00:29:15 -0700

    updated names for heroku

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -5,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/pearl-table'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/pearl-table/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
