2974df06170166a4b85780fe7c4444bfed06d3d7
Trey Crossley <treycrossley@gmail.com>
2020-07-19 00:41:23 -0700

    server again?

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/pearl-table'));
+app.use(express.static(__dirname + '/src/'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/pearl-table/index.html'));
+res.sendFile(path.join(__dirname + '/src/'));
 });
 
 // Start the app by listening on the default Heroku port
